fa6bdf76b71acabb52a4495a24b75b7d433d5b18
Jazmin M <jazminmerckel@gmail.com>
2019-10-03 01:17:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  while counter < collection.length
+    new_collection << yield(collection[counter]) #[is index, spot in array]
+    counter += 1
+  end
+  new_collection #return the new array
+end
 
+whatever_array = ["King Ghidorah", "Mecha Godzilla", "Queen Mothra"]
 
+my_collect(whatever_array) do |monster|
+  monster.split(" ").first 
+end
