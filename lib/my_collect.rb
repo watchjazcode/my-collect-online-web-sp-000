5a2e26b6901ad759ade65d164d61b49a2d435d32
Jazmin M <jazminmerckel@gmail.com>
2019-10-03 01:06:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect(collection)
+  counter = 0
+  new_collection = []
+  while counter < collection.length
+    new_collection  <<
+  end
+end
 
 
+
+
+
+
+
+
+def jaz_learns_to_code(despair)
+  amount_of_despair = 0
+  despair_jar = []
+  while amount_of_despair < despair.length
+    despair_jar << yield(despair[amount_of_despair])
+    amount_of_despair += 1
+  end
+  despair_jar
+end
+
+jaz_learns_to_code(["why", "no", "help me", "ugh", "i quit"]) { |phrase| "Jaz yells: #{phrase}" }
